c850515d6ef2d0c3378471bb9d96a1753bc3d255
Yaroslav <121808013+Mendeleev258@users.noreply.github.com>
2024-12-23 23:32:56 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: "BOBI-BOBA",
+        path: "music/bobi-boba.mp3",
+        artist: "Goga",
+        cover: "images/cover2.jpg"
+    },
+    {
+        name: "ride the lightning",
+        path: "music/ride-the-lightning.mp3",
+        artist: "Metallica",
+        cover: "images/cover1.jpg"
+    },
+    {
+        name: "Gitar",
+        path: "music/Пётр Налич - Gitar.mp3",
+        artist: "Пётр Налич",
+        cover: "images/cover3.jpg"
+    },
+    {
+        name: "Мы сидели и курили",
+        path: "music/Сплин - Мы Сидели И Курили.mp3",
+        artist: "Сплин",
+        cover: "images/cover4.jpg"
+    }
+]
